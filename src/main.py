8488ce11714098954dd89eb8d4b7e9cb5c0ee43e
Brobot64 <sulaymanibrahim64@gmail.com>
2024-11-04 10:09:19 +0100

    Trainer

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@
 
 # ```python
 import time
-from utils.scraper import scrape_exchange_rates
+from utils.scraper import scrape_exchange_rate
 from db_config import connect_db, save_rate_to_db
 import os
 from dotenv import load_dotenv
@@ -17,7 +17,7 @@ load_dotenv()
 
 def main():
     # Connect to the database
-    # conn, cursor = connect_db()
+    conn, cursor = connect_db()
 
     # Define currencies
     from_list = ['CAD', 'USD', 'GBP', 'EUR', 'CNY', 'AED']
@@ -27,16 +27,16 @@ def main():
     for from_currency in from_list:
         for to_currency in to_list:
             if from_currency != to_currency:
-                rate = scrape_exchange_rates(from_currency, to_currency)
+                rate = scrape_exchange_rate(from_currency, to_currency)
                 if rate is not None:
                     # save_rate_to_db(cursor, from_currency, to_currency, rate)
                     print(f"{from_currency} to {to_currency}: {rate}")
                 time.sleep(1)
 
     # Commit and close the connection
-    # conn.commit()
-    # cursor.close()
-    # conn.close()
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 if __name__ == "__main__":
     main()
