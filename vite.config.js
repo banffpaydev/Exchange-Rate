fce4e17e90476ec3d7ea0a48e71638f00ab3938a
dharold1 <olamidedavid10@gmail.com>
2025-01-22 17:19:35 +0100

    fixing deploy

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,10 @@
 import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path"; 
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-    port: "3004",
-    allowedHosts: ['exchange.bpay.africa'],
-  },
-  preview: {
-    host: "0.0.0.0",
-    port: "3004", // Set your desired preview port here
-    // allowedHosts: ['exchange.bpay.africa'],
-    headers: {
-      "Access-Control-Allow-Origin": "*", // Allow all origins
-    },
-    allowedHosts: "all",
-  },
   plugins: [react()],
   resolve: {
     alias: [
@@ -26,10 +12,10 @@ export default defineConfig({
         find: "@",
         replacement: fileURLToPath(new URL("./src", import.meta.url)),
       },
-      {
-        find: "lib",
-        replacement: resolve(__dirname, "lib"),
-      },
+      // {
+      //   find: "lib",
+      //   replacement: resolve(__dirname, "lib"),
+      // },
     ],
   },
 });
