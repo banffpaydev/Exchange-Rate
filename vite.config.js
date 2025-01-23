0f090c795234c3e9b6fe3655bf536eb543102141
dharold1 <olamidedavid10@gmail.com>
2025-01-23 12:03:40 +0100

    fixing deploy

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,23 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: "::",
     port: "3004",
-    allowedHosts: ['exchange.bpay.africa'],
-  },
-  preview: {
-    port: 3004, // Force Vite preview to use port 3004
-    allowedHosts: ['exchange.bpay.africa'],
+    cors: {
+      origin: "https://exchange.bpay.africa", // Allow requests from this origin
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allow specific methods
+    },
+    proxy: {
+      // Proxy requests to the host exchange.bpay.africa
+      "/api": {
+        target: "https://exchange.bpay.africa",
+        changeOrigin: true,
+        secure: false, // Set to true if you're using HTTPS with a valid certificate
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
   plugins: [react()],
   resolve: {
