6dede340dd2ffc85fd2c68189e76cca8cf8e126c
dharold1 <olamidedavid10@gmail.com>
2025-02-20 16:04:29 +0100

    fiixed rate

--- src/utils/currencySymbols.js
+++ src/utils/currencySymbols.js
@@ -1,19 +1,20 @@
 export const currencySymbols = {
-  USD: '$',
-  EUR: '€',
-  GBP: '£',
-  CAD: 'C$',
-  CNY: '¥',
-  NGN: '₦',
-  LRD: 'L$'
+  USD: "$",
+  EUR: "€",
+  GBP: "£",
+  CAD: "C$",
+  CNY: "¥",
+  NGN: "₦",
+  LRD: "L$",
 };
 
 export const formatPair = (pair) => {
-  const [base, quote] = pair.split('/');
+  const [base, quote] = pair.split("/");
   return `${currencySymbols[base]}/${currencySymbols[quote]}`;
 };
 
 export const formatDate = (dateString) => {
+  if (!dateString) return "";
   const date = new Date(dateString);
-  return date.toISOString().split('T')[0];
+  return date.toISOString().split("T")[0];
 };
